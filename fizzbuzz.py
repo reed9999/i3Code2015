ca0b0dc75127a8dc71367ba4d4e8ad03e3428bbd
Corey Jackson <cjacks04@syr.edu>
2015-06-22 16:56:58 -0400

    Made a CHange

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,3 +7,32 @@ for num in range(1,101):
         print "Buzz"
     else:
         print num
+        
+        
+# IN JAVA
+# var fizzBuzz = function () {
+# var i, output;
+# for (i = 1; i < 101; i += 1) {
+#   output = '';
+#   if (!(i % 3)) { output += 'Fizz'; }
+#   if (!(i % 5)) { output += 'Buzz'; }
+#   console.log(output || i);//empty string is false, so we short-circuit
+# }
+# };
+
+
+# IN MATLAB
+# function fizzBuzz() 
+#  for i = (1:100)
+#       if mod(i,15) == 0
+#          fprintf('FizzBuzz ')
+#       elseif mod(i,3) == 0
+#          fprintf('Fizz ')
+#       elseif mod(i,5) == 0
+#          fprintf('Buzz ')
+#       else
+#          fprintf('%i ',i)) 
+#       end
+#   end
+#   fprintf('\n');   
+# end
